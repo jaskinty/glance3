1aabfc579186b65a587690e9794a79c323d9be01
jedazip <jedazip@gmail.com>
2025-08-16 13:06:09 -0700

    force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Courses from "./courses";
 import { fetchLatestVersion } from "./lib/actions";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const json = await fetchLatestVersion();
 
