3ce37281938c82f96a8c485d76e704a81c2af70f
jedazip <jedazip@gmail.com>
2025-08-16 21:45:42 -0700

    organize links

--- app/links.tsx
+++ app/links.tsx
@@ -5,15 +5,14 @@ export default function Links() {
     <span className="flex flex-col gap-3">
       {[
         { txt: "🔑 Keys ↗️", href: "" },
-        { txt: "💭 Potential Keys ↗️", href: "" },
-        { txt: "📅 Calender ↗️", href: "" },
         { txt: "🏂 Identity Board ↗️", href: "" },
+        { txt: "📅 Calender ↗️", href: "" },
         { txt: "⛹️‍♂️ Self-Care Protocol  ↗️", href: "" },
-        { txt: "🛒 Consumables  ↗️", href: "" },
         { txt: "🎒 Belongings  ↗️", href: "" },
-        { txt: "💸 Expenses ↗️", href: "" },
         { txt: "📜 Version History ↗️", href: "/versions" },
-        { txt: "ℹ️ About this tool ↗️", href: "" },
+        { txt: "🛒 Consumables  ↗️", href: "" },
+        { txt: "💸 Expenses ↗️", href: "" },
+        { txt: "🔒 Passwords ↗️", href: "" },
       ].map(({txt, href}, index) => (
         <div key={index}>
           <Link href={href}>{txt}</Link>
